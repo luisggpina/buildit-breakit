bba00518a4dbe64ae2948a94527be9c1f7bfd929
Luis Pina <luis@luispina.me>
2014-08-18 23:03:42 -0400

    Refactored how the query results are printed

--- src/plain.ml
+++ src/plain.ml
@@ -7,6 +7,38 @@ exception Not_Implemented
 
 type mode = Plain | HTML
 
+let rec print_list lst f sep skip_last =
+  match lst with
+  | [] -> ""
+  | [ last ] -> (f last) ^ if not skip_last then sep else ""
+  | first :: rest -> (f first) ^ sep ^ (print_list rest f sep skip_last)
+
+let print_list_2 l1 l2 f g e0 =
+  let rec aux l1 l2 =
+    match l1,l2 with
+    | [] , [] -> ""
+    | e1 :: r1 , [] -> (f (g e1) e0) ^ (aux r1 [])
+    | [] , e2 :: r2 -> (f e0 (g e2)) ^ (aux [] r2)
+    | e1 :: r1 , e2 :: r2 -> (f (g e1) (g e2)) ^ (aux r1 r2) 
+  in
+  aux l1 l2
+
+let print_vs vs = print_list vs V.name "," true
+
+let print_r_v f (r,vs)=
+  match r with
+  | R.Some r -> (f (string_of_int r) (print_vs vs))
+  | _ -> ""
+
+let print_state_plain employees guests rooms =
+  (*   Print each room row *)
+  let print_r_v = print_r_v (fun r vs -> r ^ ":" ^ vs) in
+  (*   Print all room lines *)
+  let print_rs_vs lst = print_list lst print_r_v "\n" true in
+  (*   Print both visitor lines *)
+  let print_vs_vs vs_vs = print_list vs_vs print_vs "\n" true in
+  (print_vs_vs (employees :: [ guests ])) ^ (print_rs_vs rooms)
+
 let state_visitors_html_head =
 "<html>
 <body>
@@ -14,42 +46,44 @@ let state_visitors_html_head =
 <tr>
   <th>Employee</th>
   <th>Guest</th>
-</tr>"
+</tr>
+"
 
-let state_visitors_html_v e g =
+let print_v_html e g =
 "<tr>
   <td>" ^ e ^ "</td>
   <td>" ^ g ^ "</td>
-</tr>"
-
-let rec state_visitors employees guests =
-    match employees,guests with
-    | e :: e_rest , g :: g_rest -> 
-            (state_visitors_html_v (V.name e) (V.name g) ) ^ (state_visitors e_rest g_rest)
-    | e :: e_rest , []          ->
-            (state_visitors_html_v (V.name e) "") ^ (state_visitors e_rest [])
-    | [] , g :: g_rest          ->
-            (state_visitors_html_v "" (V.name g) ) ^ (state_visitors [] g_rest)
-    | [] , []                   ->
-            "\n"
+</tr>
+"
 
 let state_rooms_html_head =
 "<table>
 <tr>
   <th>Room ID</th>
   <th>Occupants</th>
-</tr>"
+</tr>
+"
 
 let state_rooms_html_tail =
 "</table>
 </body>
 </html>"
 
-let state_room_v r v =
+let print_state_room_html r v =
 "<tr>
-  <td>" ^ (string_of_int r) ^ "</td>
+  <td>" ^ r ^ "</td>
   <td>" ^ v ^ "</td>
-</tr>"
+</tr>
+"
+
+let print_state_html employees guests rooms =
+  let print_r_v = print_r_v print_state_room_html in
+  let print_rs_vs lst = print_list lst print_r_v "" false in
+  state_visitors_html_head ^
+  (print_list_2 employees guests print_v_html V.name "") ^
+  state_rooms_html_head ^
+  (print_rs_vs rooms) ^
+  state_rooms_html_tail
 
 let rec state_room_visitors visits =
     match visits with
@@ -57,60 +91,23 @@ let rec state_room_visitors visits =
     | [ last ] -> (V.name last)
     | v :: rest -> (V.name v) ^ "," ^ (state_room_visitors rest)
 
-let rec state_rooms rooms =
-    match rooms with
-    | (R.Some r, visits) :: rest ->
-            (state_room_v r (state_room_visitors visits)) ^ "\n" ^ (state_rooms rest)
-    | (_, visits) :: rest -> state_rooms rest
-    | [] -> ""
-
-let print_state head (employees, guests) mid room_lst tail =
-    print_string head ;
-    print_string (state_visitors employees guests) ;
-    print_string mid ;
-    print_string (state_rooms room_lst) ;
-    print_string tail
-
-let print_state_plain rooms =
-    let print_room (room,visitors) = 
-        match room with
-        | R.Some r -> 
-                print_int r ; 
-                print_string ":" ;
-                print_string (state_room_visitors visitors) ;
-                print_endline "" ;
-        | _ -> ()
-    in
-    let (employees, guests, room_lst) = rooms in
-    print_string (state_room_visitors employees) ;
-    print_endline "" ;
-    print_string (state_room_visitors guests) ;
-    print_endline "" ;
-    (L.iter print_room room_lst)
-
 let print_state (employees, guests, rooms) mode =
+  let s =
     match mode with
-    | HTML ->  print_state "" (employees, guests) "" rooms ""
-    | Plain -> print_state_plain (employees, guests, rooms)
+    | HTML ->  print_state_html employees guests rooms
+    | Plain -> print_state_plain employees guests rooms
+  in
+  print_string s
 
 let print_rooms_plain rooms =
-  let print_room r = print_int r ; print_string ", " in 
-  (L.iter print_room rooms) ;
-  print_endline ""
+  print_list rooms string_of_int "," false
 
 let print_rooms rooms mode =
     match mode with
     | HTML -> raise Not_Implemented
-    | Plain -> print_rooms_plain rooms
+    | Plain -> print_string (print_rooms_plain rooms)
 
-let print_names_plain names =
-  let print_visitor v = 
-      print_string ((match v with | V.Employee n | V.Guest n -> n) ^ ",")
-  in
-  (L.iter print_visitor names) ;
-  print_endline ""
-
-let print_names names mode =
+let print_names visitors mode =
     match mode with
     | HTML -> raise Not_Implemented
-    | Plain -> print_names_plain names
+    | Plain -> print_string (print_vs visitors)
